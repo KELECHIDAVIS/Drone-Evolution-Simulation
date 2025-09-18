91b28571c39242cbfeddbe0dd6c6c053d14d98fa
KELECHIDAVIS <kboss16058@gmail.com>
2025-09-18 14:13:34 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,6 @@ int main (){
     genome.addNode(NodeType::INPUT); 
     genome.addNode(NodeType::OUTPUT); 
     genome.addNode(NodeType::OUTPUT); 
-    genome.addNode(NodeType::HIDDEN); 
 
     genome.addConnection(0,6,getRandNum(-2,2), true,0); 
     genome.addConnection(0,7,getRandNum(-2,2), true,1); 
@@ -28,9 +27,8 @@ int main (){
     genome.addConnection(4,6,getRandNum(-2,2), true,8); 
     genome.addConnection(4,7,getRandNum(-2,2), true,9); 
     genome.addConnection(5,6,getRandNum(-2,2), true,10); 
-    genome.addConnection(5,7,getRandNum(-2,2), false,11); 
-    genome.addConnection(5,8,getRandNum(-2,2), true,12); 
-    genome.addConnection(8,7,getRandNum(-2,2), true,13); 
+    genome.addConnection(5,7,getRandNum(-2,2), true,11); 
+    
 
 
 
